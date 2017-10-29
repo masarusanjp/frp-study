031bf9c397be95c06ab9a710975c6c0d2db36c1d
masaichi <masarusanplusplus@gmail.com>
2017-10-29 14:53:15 +0900

    tests

---
+++ frp-studyTests/frp_studyTests.swift
@@ -0,0 +1,36 @@
+//
+//  frp_studyTests.swift
+//  frp-studyTests
+//
+//  Created by Ichikawa Masaru on 2017/09/30.
+//  Copyright © 2017年 masaichi. All rights reserved.
+//
+
+import XCTest
+@testable import frp_study
+
+class frp_studyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
